f256f0d3ed7350a80dea75f8d2b84ae9331ac66c
Vlad Holubiev <golubev.vld@gmail.com>
2017-11-01 20:48:27 +0200

    feat: process 1st level of children

--- src/index.js
+++ src/index.js
@@ -6,4 +6,31 @@ export async function chunkQuery(dbCollection, selector, chunkSize) {
   if (countBySelector <= chunkSize) {
     return [selector];
   }
+
+  return chunkChildQuery(dbCollection, selector, chunkSize, '', 1);
+}
+
+async function chunkChildQuery(dbCollection, selector, chunkSize, idPrefix, idLength) {
+  const selectorWithIdPrefix = getSelectorWithIdPrefix(selector, idPrefix);
+  const idCounts = await aggregateIdPrefixes(dbCollection, selectorWithIdPrefix, idLength);
+  const idCountsBelowChunkSize = idCounts.filter(({count}) => count <= chunkSize);
+  const idCountsAboveChunkSize = idCounts.filter(({count}) => count > chunkSize);
+  const resultChunks = [];
+
+  idCountsBelowChunkSize.forEach(({_id}) => {
+    resultChunks.push(getSelectorWithIdPrefix(selector, _id));
+  });
+
+  return resultChunks;
+}
+
+function getSelectorWithIdPrefix(selector, idPrefix) {
+  if (!idPrefix) {
+    return selector;
+  }
+
+  return {
+    ...selector,
+    _id: {$regex: `^${idPrefix}`, $options: 'i'}
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,47 @@
 jest.mock('./mongo');
 
-import {getCountBySelector, aggregateIdPrefixes} from './mongo';
+import {aggregateIdPrefixes, getCountBySelector} from './mongo';
 import {chunkQuery} from '.';
 
 it('should export chunkQuery function', () => {
   expect(chunkQuery).toBeInstanceOf(Function);
 });
 
-it('should return 1 selector if query count <= chunkSize', async() => {
-  getCountBySelector.mockReturnValueOnce(100);
-  const result = await chunkQuery({}, {a:1}, 100);
+describe('initial query count is smaller than a chunk size', () => {
+  it('should return 1 selector if query count <= chunkSize', async() => {
+    getCountBySelector.mockReturnValueOnce(100);
+    const result = await chunkQuery({}, {a: 1}, 100);
 
-  expect(result).toEqual([{a:1}]);
+    expect(result).toEqual([{a: 1}]);
+  });
+});
+
+describe('initial query is big, but next returns small chunks', () => {
+  const smallIdCountA = {_id: 'a', count: 1};
+  const smallIdCountB = {_id: 'b', count: 2};
+  const smallIdCountC = {_id: 'c', count: 3};
+  const bigIdCountA = {_id: 'z', count: 4500};
+
+  it('should call aggregateIdPrefixes when query count > chunkSize', async() => {
+    aggregateIdPrefixes.mockReturnValueOnce([smallIdCountA]);
+    getCountBySelector.mockReturnValueOnce(9999);
+    await chunkQuery({}, {a: 1}, 100);
+
+    expect(aggregateIdPrefixes).toBeCalledWith({}, {a: 1}, 1);
+  });
+
+  it('should return array of small chunked queries', async() => {
+    aggregateIdPrefixes.mockReturnValueOnce([
+      smallIdCountA, smallIdCountB, smallIdCountC
+    ]);
+    getCountBySelector.mockReturnValueOnce(9999);
+    const subQueries = await chunkQuery({}, {a: 1}, 100);
+
+    expect(subQueries).toEqual([
+      {a: 1, _id: {$regex: '^a', $options: 'i'}},
+      {a: 1, _id: {$regex: '^b', $options: 'i'}},
+      {a: 1, _id: {$regex: '^c', $options: 'i'}}
+    ]);
+  });
+
 });
